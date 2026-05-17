3bcb5cd53bc1cfe515cf83d672132a2d7ed6f1fe
hieuday88 <hieukaj@gmail.com>
2026-05-17 14:40:54 +0700

    fix(comment-service): notification with RabbitMQ

--- backend/comment-service/src/main/java/com/blog/comment/consumer/NotificationConsumer.java
+++ backend/comment-service/src/main/java/com/blog/comment/consumer/NotificationConsumer.java
@@ -2,13 +2,15 @@ package com.blog.comment.consumer;
 
 import com.blog.comment.dto.CommentEvent;
 import io.quarkus.logging.Log;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class NotificationConsumer {
     @Incoming("notifications")
-    public void consume(CommentEvent event) {
+    public void consume(JsonObject json) {
+        CommentEvent event = json.mapTo(CommentEvent.class);
         Log.infof("Received RabbitMQ event -> %s", event);
         Log.infof("Sending email notification for post %d", event.postId);
     }
